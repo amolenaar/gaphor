d839ffdeb9c81b6f82e350823581b9814f41f203
Arjan Molenaar <gaphor@gmail.com>
2021-09-28 11:53:24 +0200

    Attempt to fix locales on Windows - to no avail
    UI files still are not translated.

--- gaphor/i18n.py
+++ gaphor/i18n.py
@@ -7,21 +7,51 @@ __all__ = ["gettext"]
 import importlib.resources
 import locale
 import logging
+import sys
 from typing import Callable
 
 log = logging.getLogger(__name__)
 
 gettext: Callable[[str], str]
 
-try:
+locale.setlocale(locale.LC_ALL, "")
+
+if sys.platform == "win32":
+
+    import ctypes
+    import ctypes.util
+    import gettext as _gettext
+    import os
+
+    if not os.getenv("LANGUAGE"):
+        lang, enc = locale.getdefaultlocale()
+        os.environ["LANGUAGE"] = lang  # type: ignore[assignment]
+        result = ctypes.windll.kernel32.SetEnvironmentVariableW("LANGUAGE", lang)
+        ctypes.cdll.msvcrt._putenv(f"LANGUAGE={lang}")
+        print("Language is:", lang)
+
     with importlib.resources.path("gaphor", "__init__.py") as path:
         localedir = path.parent / "locale"
+
+        lang, enc = locale.getdefaultlocale()
+        # For GTK Builder UI:
+        libintl = ctypes.cdll.intl
+        libintl.libintl_setlocale(locale.LC_ALL, "nl-NL")
+        print(dir(libintl))
+        libintl.libintl_bindtextdomain("gaphor", str(localedir).replace("\\", "/"))
+        libintl.libintl_textdomain("gaphor")
+        libintl.libintl_bind_textdomain_codeset("gaphor", "UTF-8")
+        # For Python text:
+        translate = _gettext.translation("gaphor", localedir=localedir, fallback=True)
+        gettext = translate.gettext
+    print("gettext is", gettext)
+
+else:
+
+    with importlib.resources.path("gaphor", "__init__.py") as path:
+        localedir = path.parent / "locale"
+        locale.setlocale(locale.LC_ALL, "")
         locale.bindtextdomain("gaphor", localedir)  # type: ignore[attr-defined]
         locale.textdomain("gaphor")  # type: ignore[attr-defined]
+        locale.bind_textdomain_codeset("gaphor", "UTF-8")
         gettext = locale.gettext  # type: ignore[attr-defined]
-
-except OSError as e:
-    log.warning(f"No translations were found: {e}")
-
-    def gettext(s):
-        return s
